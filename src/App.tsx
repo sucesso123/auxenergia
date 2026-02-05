c5ebc41a827491994e0dcdb6e9cc7336f11a27cc
sucesso123 <walkerque300@gmail.com>
2026-02-05 12:13:31 -0300

    Update WhatsApp number in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { FileText, HandshakeIcon, MessageCircle } from 'lucide-react';
 
 function App() {
-  const whatsappNumber = '5511954713145';
+  const whatsappNumber = '5511949856528';
 
   const handleWhatsAppClick = (message: string) => {
     const encodedMessage = encodeURIComponent(message);
